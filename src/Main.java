cdf47633afce7f19b85302ee83a67602469532de
Alik <alikharutyunyan00@gmail.com>
2022-11-09 16:40:50 +0200

    just to check

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Start");
         printNumbers();
-        System.out.println("Finish2!");
+        System.out.println("Finish3!");
     }
 
     public static void printNumbers() {
