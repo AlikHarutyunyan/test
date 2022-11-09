ff547253b3cfd0328d7c90315f6b1290cdfe9403
Alik <alikharutyunyan00@gmail.com>
2022-11-09 16:31:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        printNumbers();
+        System.out.println("Finish!");
+    }
+
+    public static void printNumbers() {
+        for(int i =0; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
